3c253ee8d08b614073f21ba83b1e21ac2d99f0b7
jeremy <jeremy.guillaume@centralesupelec.fr>
2024-11-13 11:18:55 +0100

    Resolved merge conflict between sobel and mean filter changes

--- src/sobel.c
+++ src/sobel.c
@@ -14,30 +14,40 @@
 
 #include "sobel.h"
 
+// Sobel Filter
+void sobel(int width, int height, unsigned char *input, unsigned char *output) {
+  int i, j;
+  for (j = 1; j < height - 1; j++) {
+    for (i = 1; i < width - 1; i++) {
+      int gx = -input[(j - 1) * width + i + 1] - 2 * input[j * width + i - 1] -
+               input[(j + 1) * width + i - 1] + input[(j - 1) * width + i + 1] +
+               2 * input[j * width + i + 1] + input[(j + 1) * width + i + 1];
+      int gy = -input[(j - 1) * width + i - 1] -
+               2 * input[(j - 1) * width + i] - input[(j - 1) * width + i + 1] +
+               input[(j + 1) * width + i - 1] + 2 * input[(j + 1) * width + i] +
+               input[(j + 1) * width + i + 1];
+      output[(j - 1) * width + i] = (gx * gx + gy * gy) / 8;
+    }
+  }
+}
+
+// Mean Filter
 void mean_filter(int width, int height, unsigned char *input, unsigned char *output) {
   int i, j;
-
-  // Appliquer le filtre moyenneur
   for (j = 1; j < height - 1; j++) {
     for (i = 1; i < width - 1; i++) {
       int sum = 0;
-
-      // Calculer la somme des pixels voisins dans une fenêtre 3x3
       for (int y = -1; y <= 1; y++) {
         for (int x = -1; x <= 1; x++) {
           sum += input[(j + y) * width + (i + x)];
         }
       }
-
-      // Calculer la moyenne des pixels voisins et l'assigner à la sortie
-      output[j * width + i] = sum / 9; // 9 car il y a 9 pixels dans la fenêtre 3x3
+      output[j * width + i] = sum / 9;
     }
   }
-
-  // Remplir les bords gauche et droit avec des pixels noirs (0)
   for (j = 0; j < height; j++) {
-    output[j * width] = 0;             // Bord gauche
-    output[(j + 1) * width - 1] = 0;   // Bord droit
+    output[j * width] = 0;
+    output[(j + 1) * width - 1] = 0;
   }
 }
 
